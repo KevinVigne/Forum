35060dbd2a10f30b80ec7a681187dd97146a2634
KevinVigne <kev.vigne@gmail.com>
2025-10-23 16:55:40 +0200

    CRUD Commentaire et Topic

---
+++ src/Form/TopicType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Topic;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class TopicType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'text-color'],
+                'label' => 'Titre',
+            ])
+            ->add('description', TextType::class, [
+                'attr' => ['class' => 'text-color'],
+                'label' => 'Titre',
+            ])
+            // ->add('user', EntityType::class, [
+            //     'class' => User::class,
+            //     'choice_label' => 'id',
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Topic::class,
+        ]);
+    }
+}
